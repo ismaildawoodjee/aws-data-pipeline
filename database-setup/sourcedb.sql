d79aca5d7b3b6a97e29e46bead4356ceb38ab0cf
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2021-12-31 22:29:23 +0800

    :star: Postgres source database setup

---
+++ database-setup/sourcedb.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA IF NOT EXISTS threats;
+
+DROP TABLE IF EXISTS threats.malware_file;
+
+CREATE TABLE threats.malware_file (
+  time_received TIMESTAMP,
+  download_source TEXT,
+  top_level_domain TEXT,
+  download_speed TEXT,
+  ping_time_to_server INT,
+  file_size_bytes NUMERIC,
+  how_many_times_file_seen INT,
+  executable_code_maybe_present_in_headers TEXT,
+  calls_to_low_level_system_libraries NUMERIC,
+  evidence_of_code_obfuscation TEXT,
+  threads_started INT,
+  mean_word_length_of_extracted_strings NUMERIC,
+  similarity_score NUMERIC,
+  characters_in_url INT,
+  actually_malicious TEXT,
+  initial_statistical_analysis TEXT
+);
+
+COPY threats.malware_file
+FROM
+  '/source-data/dated_sample.csv' CSV DELIMITER ',' HEADER;
