809663d397e7b9d0aaffbb7612e5e34b7aa715e0
Ismail Dawoodjee <58870992+ismaildawoodjee@users.noreply.github.com>
2022-01-15 19:16:07 +0800

    :star: Define ostgres foreign data wrapper and dblink functions for Postgres-Postgres data movement

---
+++ database-setup/supersetdb.sql
@@ -0,0 +1,37 @@
+-- Only ran once. Putting column names in double-quotes allow Postgres to capitalize them.
+DROP TABLE IF EXISTS public.malware_file;
+
+CREATE TABLE public.malware_file (
+    "TimeReceived" TIMESTAMP,
+    "DownloadSource" TEXT,
+    "TopLevelDomain" TEXT,
+    "PingTimeToServer" INT,
+    "FileSizeBytes" NUMERIC,
+    "ExecutableCodeMaybePresentInHeaders" TEXT,
+    "CallsToLowLevelSystemLibraries" NUMERIC,
+    "EvidenceOfCodeObfuscation" TEXT,
+    "ThreadsStarted" INT,
+    "CharactersInUrl" INT,
+    "ActuallyMalicious" TEXT,
+    "InitialStatisticalAnalysis" TEXT
+);
+
+-- The following code creates a `dblink` function in order to load data from
+-- Redshift into a Postgres database running on EC2. Also ran only once.
+-- This will ensure that daily data from Redshift is available in an always-online
+-- database, even when Redshift is paused.
+CREATE EXTENSION postgres_fdw;
+
+CREATE EXTENSION dblink;
+
+CREATE SERVER foreign_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (
+    host 'MY_REDSHIFT_ENDPOINT',
+    port '5439',
+    dbname 'malwaredb',
+    sslmode 'require'
+);
+
+CREATE USER MAPPING FOR postgres SERVER foreign_server OPTIONS (
+    user 'MY_REDSHIFT_CLUSTER_USERNAME',
+    password 'MY_REDSHIFT_CLUSTER_PASSWORD'
+);
